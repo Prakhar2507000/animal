f8fb27156669337cede161a386b3e92c23e64244
Prakhar2507000 <151045819+Prakhar2507000@users.noreply.github.com>
2024-06-17 19:52:18 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+
+https://teachablemachine.withgoogle.com/models/VFWtXAEId/
+
+<img src="cat.gif" id="Cat" width="300" height="300">
+<img src="dog.gif" id="Dog" width="300" height="300">
+<img src="bird.gif" id="Bird" width="300" height="300"></img>
